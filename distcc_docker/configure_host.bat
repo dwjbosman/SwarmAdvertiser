dbedda0939bc20ab72b2cf289c9d2e32e5326ab2
Dinne Bosman <dwjbosman@yahoo.com>
2023-07-25 22:14:33 +0200

    Reorganize

---
+++ distcc_docker/configure_host.bat
@@ -0,0 +1,12 @@
+netsh advfirewall firewall add rule name="node_communication_tcp" dir=in action=allow protocol=TCP localport=7946
+netsh advfirewall firewall add rule name="node_communication_udp" dir=in action=allow protocol=UDP localport=7946
+netsh advfirewall firewall add rule name="overlay_network" dir=in action=allow protocol=UDP localport=4789
+netsh advfirewall firewall add rule name="swarm_dns_tcp" dir=in action=allow protocol=TCP localport=53
+netsh advfirewall firewall add rule name="swarm_dns_udp" dir=in action=allow protocol=UDP localport=53
+netsh advfirewall firewall add rule name="cluster_management" dir=in action=allow protocol=UDP localport=2377
+netsh advfirewall firewall add rule name="cluster_management" dir=in action=allow protocol=TCP localport=2377
+
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=7946 connectaddress=$(wsl hostname -I) connectport=7946
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=53 connectaddress=$(wsl hostname -I) connectport=53
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2377 connectaddress=$(wsl hostname -I) connectport=2377
+
